9e94dce16f857097311d372df660c8f11eeeb813
YXY <932687738@qq.com>
2022-04-03 07:36:54 +0800

    init react study

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React,{Component} from "react";
+import Hello from "./Hello";
+
+export default class APP extends Component{
+    render() {
+        return(
+            <Hello/>
+        )
+    }
+}
